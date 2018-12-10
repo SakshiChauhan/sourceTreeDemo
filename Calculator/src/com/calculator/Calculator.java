300e3e7407565ef29420fc4d1f77fd5ac011bed2
sakshichauhan11 <sakshi.uiet@gmail.com>
2018-12-10 11:44:02 +0530

    subtraction function

--- Calculator/src/com/calculator/Calculator.java
+++ Calculator/src/com/calculator/Calculator.java
@@ -8,10 +8,15 @@ public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Calcualtor service.");
 		System.out.println(sum());
+		System.out.println(sub());
 	}
 	
 	public static int sum(){
 		return 3+5;
+	}
+	
+	public static int sub(){
+		return 10-3;
 	}
 
 }
