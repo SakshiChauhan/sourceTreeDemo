7ca20c944387472b97a23608e231115bcc7a0a49
sakshichauhan11 <sakshi.uiet@gmail.com>
2018-12-10 11:46:47 +0530

    multiply function

--- Calculator/src/com/calculator/Calculator.java
+++ Calculator/src/com/calculator/Calculator.java
@@ -7,6 +7,11 @@ public class Calculator {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Calcualtor service.");
+		System.out.println(multiply());
+	}
+	
+	public static int multiply(){
+		return 6*4;
 	}
 
 }
